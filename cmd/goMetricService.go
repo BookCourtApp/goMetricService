b78d90f979610b46a3ea8db68c1908127d383867
Alex Fomin <alexkillzone53@gmail.com>
2023-09-04 00:35:49 +1000

    refactor: replace init code to main file
    Initialization of application was in app package, but it is
    considered to have all initialization code in main file,
    therefore such decision has been made.

--- cmd/goMetricService.go
+++ cmd/goMetricService.go
@@ -5,8 +5,21 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/wanna-beat-by-bit/goMetricService/internal/app"
+	"fmt"
+	"time"
+
+	//"github.com/wanna-beat-by-bit/goMetricService/internal/app"
+	//"github.com/wanna-beat-by-bit/goMetricService/internal/app/config"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/wanna-beat-by-bit/goMetricService/internal/app/config"
+	"github.com/wanna-beat-by-bit/goMetricService/internal/app/http-server/handlers/saveHandler"
+	"github.com/wanna-beat-by-bit/goMetricService/internal/app/http-server/middleware/logging"
+	"github.com/wanna-beat-by-bit/goMetricService/internal/app/http-server/server"
+	"github.com/wanna-beat-by-bit/goMetricService/internal/app/service"
+	"github.com/wanna-beat-by-bit/goMetricService/internal/app/storage/clickhouse"
+	RedisCache "github.com/wanna-beat-by-bit/goMetricService/internal/app/storage/redis"
 )
 
 const (
@@ -23,9 +36,9 @@ func main() {
 		"Logger enabled",
 		slog.String("env", conf.Env),
 	)
-	logger.Debug("debug messages is enabled")
+	logger.Debug("debug messages enabled")
 
-	a, err := app.New(logger, conf)
+	a, err := New(logger, conf)
 	if err != nil {
 		logger.Error("fatal error occured", slog.String("error", err.Error()))
 		panic("can't build application")
@@ -66,4 +79,81 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return logger
+}
+
+type App struct {
+	config  *config.Config
+	srv     *server.Server
+	storage *clickhouse.Clickhouse
+	cache   *RedisCache.SessionCache
+	logger  *slog.Logger
+}
+
+func New(logger *slog.Logger, conf *config.Config) (*App, error) {
+	const op = "app.New"
+	var err error
+
+	a := &App{}
+	a.logger = logger
+	a.config = conf
+
+	appLog := a.logger.With(
+		slog.String("op", op),
+	)
+	appLog.Debug("Building started")
+
+	appLog.Debug("Creating database")
+	a.storage, err = clickhouse.New(a.config)
+	if err != nil {
+		return nil, fmt.Errorf("error while create creating database: %s", err.Error())
+	}
+
+	//appLog.Debug("Initializing database")
+	//if err := a.storage.Init(); err != nil {
+	//	return nil, fmt.Errorf("error while intializing clickhouse: %s", err.Error())
+	//}
+
+	appLog.Debug("Creating session caching")
+	a.cache, err = RedisCache.New(a.config)
+	if err != nil {
+		return nil, fmt.Errorf("error while initializing redis: %s", err.Error())
+	}
+
+	srvc := service.New(a.logger, a.storage, a.cache)
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Recoverer)
+	router.Use(logging.New(logger))
+	router.Post("/test", saveHandler.New(srvc))
+
+	a.srv = server.New(a.config, router)
+
+	appLog.Debug("Building finished")
+
+	return a, nil
+}
+
+func (a *App) Run() error {
+	a.logger.Info("Server is up", slog.String("IP", a.srv.Network.Addr))
+
+	if err := a.srv.Network.ListenAndServe(); err != nil {
+		return fmt.Errorf("error occured while in server: %s", err.Error())
+	}
+
+	return nil
+}
+
+func (a *App) Stop(ctx context.Context) error {
+	const op = "app.Stop"
+	a.logger.Info("Stopping the server",
+		slog.String("op", op),
+	)
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+	if err := a.srv.Network.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error while stopping server gracefully: %s", err.Error())
+	}
+
+	return nil
 }
